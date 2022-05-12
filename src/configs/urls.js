47cd92bbe646daa2ad4e43750e31ad0603b7afea
Taisiia Valieva <taya.skachkova@gmail.com>
2022-05-12 16:21:16 +0100

    feat/added city search api

--- src/configs/urls.js
+++ src/configs/urls.js
@@ -1,3 +1,1 @@
-export const API_WEATHER_URL =
-  "https://api.openweathermap.org/data/2.5/onecall?";
-export const API_CITY_URL = "http://api.openweathermap.org/geo/1.0/direct?q=";
+export const API_ROOT_URL = "https://api.openweathermap.org/";
