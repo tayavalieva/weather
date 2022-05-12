f575a178b3c2413e559ef565bc27deb0e6648272
Taisiia Valieva <taya.skachkova@gmail.com>
2022-05-12 18:31:53 +0100

    refactor: app styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,16 +3,14 @@ import { apiCitySearch } from "../../utils/api-city";
 import Input from "../Input/Input";
 
 function App() {
-  apiCitySearch.getCity("London").then((data) => console.log(data));
+  // apiCitySearch.getCity("London").then((data) => console.log(data));
 
   const cities = [{ label: "Berlin" }, { label: "London" }, { label: "Berd" }];
   return (
-    <div className='App'>
-      <header className='App-header'>header</header>
+    <div className='app'>
       <div>
         <Input cities={cities} />
       </div>
-      <footer>Footer</footer>
     </div>
   );
 }
