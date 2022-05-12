00ec2daa2d200545d728b0c6f523f9f6925e2e4b
Taisiia Valieva <taya.skachkova@gmail.com>
2022-05-12 11:09:30 +0100

    feat/CRA initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>header</header>
+      <div>Hello</div>
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+export default App;
