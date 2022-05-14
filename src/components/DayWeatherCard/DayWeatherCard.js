38b6177cdb4f0cfd4082b6849adc3ed543cc37c7
Taisiia Valieva <taya.skachkova@gmail.com>
2022-05-14 16:53:57 +0100

    feat: added weather conditions icons

--- src/components/DayWeatherCard/DayWeatherCard.js
+++ src/components/DayWeatherCard/DayWeatherCard.js
@@ -1,13 +1,19 @@
 import "./DayWeatherCard.css";
 import { Card } from "@mui/material";
 import convertUnixTime from "../../utils/date-convertor";
+import { ICONS_URL } from "../../configs/urls";
 
 const DayWeatherCard = ({ dayWeather }) => {
   const date = convertUnixTime(dayWeather["dt"]);
+  const weatherConditions = dayWeather.weather[0];
+
   return (
     <Card className='daily-weather'>
       <p>{date}</p>
-      <p>{dayWeather["weather"][0]["main"]}</p>
+      <img
+        src={`${ICONS_URL}${weatherConditions.icon}.png`}
+        alt={weatherConditions.main}
+      />
       <p>{`${dayWeather["temp"]["min"].toFixed()} °C`}</p>
       <span>-</span>
       <p>{`${dayWeather["temp"]["max"].toFixed()} °C`}</p>
